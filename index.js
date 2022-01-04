17a7bacca8f810cb2b62c6c41994a61b74e5e281
Sathya7411 <97081152+Sathya7411@users.noreply.github.com>
2022-01-03 22:36:27 -0800

    intial commit 1

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const express = require("express");
-var mongoose= require("mongoose");
+const express = require("express")
+var mongoose = require("mongoose");
 const router = express.Router();
-var app = express();
+var app = express()
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var deptUi = require('./models/department_Info.js');
-var deptApi = require('./routes/deprtment_Api.js');
+var department = require('./models/department.js');
+var departmentApi = require('./router/department-Api.js');
 
-var stdUi= require('./models/student_Info.js');
-var stdApi= require('./routes/student_Api.js');
+var student = require('./models/student_info.js');
+var studentApi = require('./router/student_info-Api.js');
 
-var courseUi= require('./models/course_Info.js');
-var courseApi=require('./routes/course_Api.js');
+var course = require('./models/course_info.js');
+var courseApi = require('./router/course_info-Api.js');
 
-var markUi=require('./models/mark_Info.js');
-var markApi=require('./routes/mark_Info.js');
+var marksModel = require('./models/marks_info.js');
+var marksApi = require('./router/marks_info-Api.js');
 
-app.use('/',router);
+const {application} = require("express");
+
+app.use('/', router);
 app.get("/",function(request,response){
-    response.send("hello world")
+response.send("Hello World!")
 })
-mongoose.connect('mongodb://localhost:27017/studentsDB',()=> {console.log("successfully connected to database");});
-app.listen(2000,function(){
-    console.log("started application on port %d", 2000)
-});
-app.use('/',deptApi);
-app.use('/',stdApi);
-app.use('/',courseApi);
-app.use('/',markApi);
 
+mongoose.connect('mongodb://localhost:27017/studentmanagement', () => { console.log("[+] Succesfully connected to database."); });
+app.listen(3000, function () {
+console.log("Started application on port %d", 3000);
+});
+
+app.use('/', departmentApi);
+app.use('/', studentApi);
+app.use('/', courseApi); 
+app.use('/', marksApi); 
